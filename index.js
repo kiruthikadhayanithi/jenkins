a4a99f194e3950111a24c8b520fd33806b87c358
kiruthika dhayanithi <kiruthika.dhayanithi@twilightsoftwares.com>
2022-11-04 10:54:29 +0530

    fix: express

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+const express = require('express');
+const app = express();
+const port = 3000;
 
-  console.log(" Slack testing");
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+  console.log("Hello Jenkins");
+});
 
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
