5188125e8e7000a8bc5e3641290c403fc1fe2ef3
kiruthika dhayanithi <kiruthika.dhayanithi@twilightsoftwares.com>
2022-11-12 10:54:40 +0530

    fix: Testing for sonarqube

--- index.js
+++ index.js
@@ -1,29 +1,13 @@
 const express = require("express");
 const app = express();
 
-
-
-
 const port = 3000;
 
-
-
-
 app.get("/", (req, res) => {
   res.send("Hello Worldlkllkjfklk!");
 });
 
 console.log("Hello Jenkin");
-
-
-app.get("/", (req, res) => {
-  res.send("Hello Worldlkllkjfklk!");
-});
-
-console.log("Hello Jenkin");
-
-
-
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
