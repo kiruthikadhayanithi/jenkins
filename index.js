b495793a77f8687738ffe7de75486ecffc1ddb33
fernandus <fernandus.moris@broadcom.com>
2022-12-02 12:40:18 +0530

    fix: Testing husky

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const app = express();
 
 const port = 7000;
 
-app.get('/', (req, res) => {
-  res.send('Hello Wk!');
-});
+            app.get('/', (req, res) => {
+              res.send('Hello Wk!');
+            });
 
 app.listen(port, () => {
   console.log(`Example listening on port ${port}`);
